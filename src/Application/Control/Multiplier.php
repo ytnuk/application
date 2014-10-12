b38136c69b6bdd304ab0aff8b74c377bcd369e46
Vít Kutný <vitkutny@outlook.com>
2014-10-12 19:06:28 +0200

    dev

--- src/Application/Control/Multiplier.php
+++ src/Application/Control/Multiplier.php
@@ -6,5 +6,8 @@ use Nette\Application;
 
 final class Multiplier extends Application\UI\Multiplier
 {
-
+    public function __construct(callable $factory)
+    {
+        parent::__construct($factory);
+    }
 }
