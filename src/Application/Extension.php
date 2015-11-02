b7789eee85999e97ff3b7840ac01dad1199c8fa8
Vít Kutný <vitkutny@icloud.com>
2015-11-03 00:37:19 +0100

    Extension refactor

--- src/Application/Extension.php
+++ src/Application/Extension.php
@@ -2,36 +2,43 @@
 namespace Ytnuk\Application;
 
 use Nette;
-use Ytnuk;
 
 final class Extension
 	extends Nette\DI\CompilerExtension
-	implements Ytnuk\Config\Provider
 {
 
-	public function getConfigResources() : array
+	public function loadConfiguration()
 	{
-		return [
-			Nette\Bridges\ApplicationDI\ApplicationExtension::class => [
-				'errorPresenter' => NULL,
-				'mapping' => [
-					'*' => 'Ytnuk\*\*',
-				],
-				'scanDirs' => FALSE,
-			],
-			Nette\Bridges\HttpDI\SessionExtension::class => [
-				'debugger' => TRUE,
-			],
-			Nette\DI\Extensions\DIExtension::class => [
-				'debugger' => TRUE,
-			],
-			Nette\DI\Extensions\DecoratorExtension::class => [
-				Control::class => [
-					'setup' => [
-						'setCacheStorage',
-					],
-				],
-			],
-		];
+		parent::loadConfiguration();
+		$application = current($this->compiler->getExtensions(Nette\Bridges\ApplicationDI\ApplicationExtension::class));
+		if ($application instanceof Nette\Bridges\ApplicationDI\ApplicationExtension) {
+			$application->defaults['errorPresenter'] = FALSE;
+			$application->defaults['scanDirs'] = FALSE;
+			$application->defaults['mapping'] = [
+				'*' => 'Ytnuk\*\*',
+			];
+		}
+		$session = current($this->compiler->getExtensions(Nette\Bridges\HttpDI\SessionExtension::class));
+		if ($session instanceof Nette\Bridges\HttpDI\SessionExtension) {
+			$session->defaults['debugger'] = TRUE;
+		}
+		$di = current($this->compiler->getExtensions(Nette\DI\Extensions\DIExtension::class));
+		if ($di instanceof Nette\DI\Extensions\DIExtension) {
+			$di->defaults['debugger'] = TRUE;
+		}
+	}
+
+	public function beforeCompile()
+	{
+		parent::beforeCompile();
+		$decorator = current($this->compiler->getExtensions(Nette\DI\Extensions\DecoratorExtension::class));
+		if ($decorator instanceof Nette\DI\Extensions\DecoratorExtension) {
+			$decorator->addSetups(
+				Control::class,
+				[
+					'setCacheStorage',
+				]
+			);
+		}
 	}
 }
