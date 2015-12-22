0e7e07a7c82902adc516a580be53edb0db8deac3
Vít Kutný <vitkutny@icloud.com>
2015-12-22 03:16:13 +0100

    extension debugMode

--- src/Application/Extension.php
+++ src/Application/Extension.php
@@ -8,6 +8,16 @@ final class Extension
 	extends Nette\DI\CompilerExtension
 {
 
+	/**
+	 * @var bool
+	 */
+	private $debugMode;
+
+	public function __construct(bool $debugMode = FALSE)
+	{
+		$this->debugMode = $debugMode;
+	}
+
 	public function beforeCompile()
 	{
 		parent::beforeCompile();
@@ -37,16 +47,18 @@ final class Extension
 		}
 		$session = current($compiler->getExtensions(Nette\Bridges\HttpDI\SessionExtension::class));
 		if ($session instanceof Nette\Bridges\HttpDI\SessionExtension) {
-			$session->defaults['debugger'] = TRUE;
+			$session->defaults['debugger'] = $this->debugMode;
 		}
 		$http = current($compiler->getExtensions(Nette\Bridges\HttpDI\HttpExtension::class));
 		if ($http instanceof Nette\Bridges\HttpDI\HttpExtension) {
 			$http->defaults['headers']['X-Powered-By'] = __NAMESPACE__;
-			$http->defaults['headers']['Content-Security-Policy'] = 'default-src \'self\'; form-action \'self\'';
+			if ( ! $this->debugMode) {
+				$http->defaults['headers']['Content-Security-Policy'] = 'default-src \'self\'; form-action \'self\'';
+			}
 		}
 		$di = current($compiler->getExtensions(Nette\DI\Extensions\DIExtension::class));
 		if ($di instanceof Nette\DI\Extensions\DIExtension) {
-			$di->defaults['debugger'] = TRUE;
+			$di->defaults['debugger'] = $this->debugMode;
 		}
 
 		return parent::setCompiler(
